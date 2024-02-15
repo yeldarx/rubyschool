699dded21e0be28c9861223e351c8e59c7fdf8db
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-15 08:04:05 +0600

    write "your age" app

---
+++ book/your-age.rb
@@ -0,0 +1,9 @@
+puts "What's your name?"
+name = gets
+
+puts "Hello, #{name}!"
+puts "How old are you?"
+age = gets
+puts "Your name is #{name} and you #{age} years old"
+
+# после конкатенации начинается с новой строки, нужно чтобы было в одной строке
