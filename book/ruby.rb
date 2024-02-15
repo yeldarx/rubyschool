0acb31ab7f3bf557403ecb6546d0a746b10d6b7c
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-15 07:47:07 +0600

    add conversation puts + gets

--- book/ruby.rb
+++ book/ruby.rb
@@ -1,3 +1,16 @@
 # first exer
 
 puts "Hello, World!"
+    gets
+
+puts "How do you do?"
+    gets
+
+puts "Me too"
+    gets
+
+puts "I'm your first app that you write on Ruby"
+    gets
+
+puts "Welcome to Ruby World!"
+    gets
