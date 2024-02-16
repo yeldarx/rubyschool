18e6dbf3e15f0a440a9198ed3efb1c1ec9669051
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-17 02:48:48 +0600

    salary, savings calculator

---
+++ book/salary.rb
@@ -0,0 +1,4 @@
+puts "Your salary?"
+salary = gets.to_i
+savings = salary * 15 / 100
+puts "It means that your yearly salary is #{salary * 12} and your monthly savings is #{salary * 15 / 100}!. In five years it will be #{savings * 5}!"
