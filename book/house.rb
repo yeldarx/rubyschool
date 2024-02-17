997ca0440962d7cba5f46470d46f9dfda432c787
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-18 03:05:46 +0600

    house exer v2

--- book/house.rb
+++ book/house.rb
@@ -1,12 +1,11 @@
 house_price = 500000
-will_be_paid = 500000
-ins_years = 30
+will_be_paid = house_price * 0.04
+ins_years = 1
+price_for_one_year = 16666
 
-price_for_one_year = house_price / ins_years
-
-ins_years.times do
-    puts "Осталось годов: #{ins_years}, еще нужно оплатить: #{will_be_paid}"
-    will_be_paid = house_price - price_for_one_year
-    ins_years = ins_years - 1
-    price_for_one_year = price_for_one_year + 16660
+30.times do
+    puts "#{ins_years} год, за использование кредита нужно заплатить #{will_be_paid.to_i} долларов США"
+    house_price = house_price - price_for_one_year
+    ins_years = ins_years + 1
+    will_be_paid = house_price * 0.04
 end
