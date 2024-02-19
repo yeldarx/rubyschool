2665531c103e3d62e5bd6805dca34a275c09f3c0
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-19 12:52:45 +0600

    house v2

--- book/house.rb
+++ book/house.rb
@@ -2,10 +2,13 @@ house_price = 500000
 will_be_paid = house_price * 0.04
 ins_years = 1
 price_for_one_year = 16666
+total_percent = 0 
 
 30.times do
-    puts "#{ins_years} год, за использование кредита нужно заплатить #{will_be_paid.to_i} долларов США"
+    puts "#{ins_years} год, за использование кредита нужно заплатить #{will_be_paid.to_i} долларов США."
     house_price = house_price - price_for_one_year
     ins_years = ins_years + 1
     will_be_paid = house_price * 0.04
+    total_percent = total_percent + will_be_paid
 end
+puts "Общая сумма процентов: #{total_percent}"
