234da9cd8e171b8b9fdb8545bddb2489dbbd107a
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-25 21:19:03 +0600

    made the animation more interesting

--- book/slot-machine.rb
+++ book/slot-machine.rb
@@ -3,58 +3,62 @@
 
 # if age < 18
 #     puts "Нет 18 - нет игры! Сау болш!"
-#     # exit # нужен только для того, чтобы остановить бесконечный цикл?
+#     exit # нужен только для того, чтобы остановить бесконечный цикл?
 # end
 
-def progress_bar
-    times_rand = rand(1..2)
-    times_rand.times do
-        print "/\r"
-        sleep 0.1
-    
-        print "-\r"
-        sleep 0.1
-        
-        print "\\\r"
-        sleep 0.1
-    
-        print "|\r"
-        sleep 0.1 
-    end 
-end
-
-balance = 5
+balance = 10
 
 loop do
     puts 'Нажмите Enter, чтобы дернуть ручку...'
     gets # очень интересный прием
 
-    x = rand(1..3)
-    y = rand(1..4)
-    z = rand(1..5)
+    sleep 0.3
 
-    progress_bar
-    
-    puts "Результат: #{x} #{y} #{z}"
+    x = rand(1..3)
+    y = rand(1..3)
+    z = rand(1..3)
+
+    print "Результат: " # цифры появляются аниминованно
+    sleep rand(0.5..1.5)
+    print "#{x} "
+    sleep rand(0.5..1.5)
+    print "#{y} "
+    sleep rand(0.5..1.5)
+    print "#{z}"
+
+    sleep 1
+
+    puts
+    puts
+
 
     if x == 0 && y == 0 && z == 0
         balance = 0
     elsif x == 1 && y == 1 && z == 1
         balance += 10
-        puts 'Баланс увеличился на 10 долларов'
+        puts 'Баланс увеличился на 10 долларов.'
     elsif x == 2 && y == 2 && z == 2
         balance += 20
-        puts 'Баланс увеличился на 20 долларов'
+        puts 'Баланс увеличился на 20 долларов.'
+    elsif x == 3 && y == 3 && z == 3
+        balance += 30
+        puts 'Баланс увеличился на 30 долларов.'
     else
         balance -= 0.5
-        puts 'Баланс уменьшился на 50 центов'
+        puts "Баланс уменьшился на 50 центов и равен #{balance} долларам США."
+
+        if balance <= 0
+            puts
+            puts "Стоп-игра."
+            exit
+        end
+
     end
 
-    if balance <= 0
-        puts "Ваш баланс равен или меньше нуля - стоп-игра."
-        exit
-    elsif balance > 0
-        puts "Ваш баланс равен #{balance}$, продолжайте дергать..."
-    end
+    puts
+    
+    # sleep 0.5
+    
+    
 
 end
