3c270fd32f56bac76b75db91416f00abb25a6caf
Yeldar Kudaibergenov <mail@yeldar.org>
2024-03-01 00:16:07 +0500

    phone converter v1
    “Задание 3
    
    К вам обратился предприниматель Джон Смит. Джон говорит, что его бизнес специализируется на создании телефонных номеров для рекламы. Они хотят подписать с вами контракт, но прежде хотелось бы убедиться, что вы хороший программист, можете работать с их требованиями и доставлять качественное программное обеспечение. Они говорят: у нас есть номера телефонов с буквами. Например, для бизнеса по продаже матрасов существует номер «555-MATRESS», который транслируется в «555-628-7377». Когда наши клиенты набирают буквенный номер на клавиатуре телефона (см. картинку ниже), он транслируется в цифровой. Напишите программу, которая будет переводить (транслировать) слово без дефисов в телефонный номер. Сигнатура метода будет следующей:
    
     def phone_to_number(phone)
      # ваш код тут...
    end
    
    puts phone_to_number('555MATRESS') # должно напечатать 5556287377”
    
    Excerpt From: Роман Пушкин. “Руби для романтиков.” Apple Books.

--- book/phone-num-converter.rb
+++ book/phone-num-converter.rb
@@ -1,5 +1,30 @@
 def phone_conv(num)
+
+    numlen = num.length - 1
+
+    0.upto(numlen) do |i|
+        case 
+        when num[i] == 'A' || num[i] == 'B' || num[i] == 'C'
+            num[i] = '2'
+        when num[i] == 'D' || num[i] == 'E' || num[i] == 'F'
+            num[i] = '3'
+        when num[i] == 'G' || num[i] == 'H' || num[i] == 'I'
+            num[i] = '4'
+        when num[i] == 'J' || num[i] == 'K' || num[i] == 'L'
+            num[i] = '5'
+        when num[i] == 'M' || num[i] == 'N' || num[i] == 'O'
+            num[i] = '6'
+        when num[i] == 'P' || num[i] == 'Q' || num[i] == 'R' || num[i] == 'S'
+            num[i] = '7'
+        when num[i] == 'T' || num[i] == 'U' || num[i] == 'V'
+            num[i] = '8'
+        when num[i] == 'W' || num[i] == 'X' || num[i] == 'Y' || num[i] == 'Z'
+            num[i] = '9'
+        else num[i]
+        end
+    end
     num
+
 end
 
-puts phone_conv('nknk')
+puts phone_conv('1JJM__A876RE__ SS')
