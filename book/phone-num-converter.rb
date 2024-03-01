d113f61808c494facc779fdaa9991208c21e29dd
Yeldar Kudaibergenov <mail@yeldar.org>
2024-03-01 16:59:19 +0500

    phone converter v3

--- book/phone-num-converter.rb
+++ book/phone-num-converter.rb
@@ -1,64 +1,28 @@
 def phone_conv(num)
     numlen = num.length - 1
     0.upto(numlen) do |i|
-        abc = %w(A B C)
-        de_f = %w(D E F)
-        ghi = %w(G H I)
-        jkl = %w(J K L)
-        mno = %w(M N O)
-        pqrs = %w(P Q R S)
-        tuv = %w(T U V)
-        wxyz = %w(W X Y Z)
-
-        abc.each do |n|
-            if num[i].upcase == n
-                num[i] = '2'
-            end
-        end
-
-        de_f.each do |n|
-            if num[i].upcase == n
-                num[i] = '3'
-            end
-        end
-
-        ghi.each do |n|
-            if num[i].upcase == n
-                num[i] = '4'
-            end
-        end
-
-        jkl.each do |n|
-            if num[i].upcase == n
-                num[i] = '5'
-            end
-        end
-
-        mno.each do |n|
-            if num[i].upcase == n
-                num[i] = '6'
-            end
-        end
-
-        pqrs.each do |n|
-            if num[i].upcase == n
-                num[i] = '7'
-            end
-        end
-
-        tuv.each do |n|
-            if num[i].upcase == n
-                num[i] = '8'
-            end
-        end
-
-        wxyz.each do |n|
-            if num[i].upcase == n
-                num[i] = '9'
-            end
+        letters = [
+            %w(A B C),   # 0
+            %w(D E F),   # 1
+            %w(G H I),   # 2
+            %w(J K L),   # 3
+            %w(M N O),   # 4
+            %w(P Q R S), # 5
+            %w(T U V),   # 6
+            %w(W X Y Z)  # 7
+        ]
+ 
+        letters.each_with_index do |j, index_j|
+            j.each_with_index do |k, index_k|
+                if num[i].upcase == letters[index_j][index_k]
+                    newnum = index_j + 2
+                    num[i] = newnum.to_s
+                end
+            end          
         end
     end
     num
 end
 
-puts phone_conv('555-MATreSS')
+
+puts phone_conv('555-MATRESS') # => 555-6287377
