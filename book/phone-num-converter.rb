9f5159bfbe2d35feba6c209818fc8bd106a14ca3
Yeldar Kudaibergenov <mail@yeldar.org>
2024-03-01 16:02:45 +0500

    phone converter v2

--- book/phone-num-converter.rb
+++ book/phone-num-converter.rb
@@ -1,30 +1,64 @@
 def phone_conv(num)
-
     numlen = num.length - 1
-
     0.upto(numlen) do |i|
-        case 
-        when num[i] == 'A' || num[i] == 'B' || num[i] == 'C'
-            num[i] = '2'
-        when num[i] == 'D' || num[i] == 'E' || num[i] == 'F'
-            num[i] = '3'
-        when num[i] == 'G' || num[i] == 'H' || num[i] == 'I'
-            num[i] = '4'
-        when num[i] == 'J' || num[i] == 'K' || num[i] == 'L'
-            num[i] = '5'
-        when num[i] == 'M' || num[i] == 'N' || num[i] == 'O'
-            num[i] = '6'
-        when num[i] == 'P' || num[i] == 'Q' || num[i] == 'R' || num[i] == 'S'
-            num[i] = '7'
-        when num[i] == 'T' || num[i] == 'U' || num[i] == 'V'
-            num[i] = '8'
-        when num[i] == 'W' || num[i] == 'X' || num[i] == 'Y' || num[i] == 'Z'
-            num[i] = '9'
-        else num[i]
+        abc = %w(A B C)
+        de_f = %w(D E F)
+        ghi = %w(G H I)
+        jkl = %w(J K L)
+        mno = %w(M N O)
+        pqrs = %w(P Q R S)
+        tuv = %w(T U V)
+        wxyz = %w(W X Y Z)
+
+        abc.each do |n|
+            if num[i].upcase == n
+                num[i] = '2'
+            end
+        end
+
+        de_f.each do |n|
+            if num[i].upcase == n
+                num[i] = '3'
+            end
+        end
+
+        ghi.each do |n|
+            if num[i].upcase == n
+                num[i] = '4'
+            end
+        end
+
+        jkl.each do |n|
+            if num[i].upcase == n
+                num[i] = '5'
+            end
+        end
+
+        mno.each do |n|
+            if num[i].upcase == n
+                num[i] = '6'
+            end
+        end
+
+        pqrs.each do |n|
+            if num[i].upcase == n
+                num[i] = '7'
+            end
+        end
+
+        tuv.each do |n|
+            if num[i].upcase == n
+                num[i] = '8'
+            end
+        end
+
+        wxyz.each do |n|
+            if num[i].upcase == n
+                num[i] = '9'
+            end
         end
     end
     num
-
 end
 
-puts phone_conv('1JJM__A876RE__ SS')
+puts phone_conv('555-MATreSS')
