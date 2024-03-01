76e040ecee8b17d7eef960e65892b96fb353c3fd
Yeldar Kudaibergenov <mail@yeldar.org>
2024-03-01 17:51:36 +0500

    phone converter v3.1 minor improvments

--- book/phone-num-converter.rb
+++ book/phone-num-converter.rb
@@ -1,28 +1,29 @@
 def phone_conv(num)
+
+    letters = [
+            [],          # 0
+            [],          # 1
+            %w(A B C),   # 2
+            %w(D E F),   # 3
+            %w(G H I),   # 4
+            %w(J K L),   # 5
+            %w(M N O),   # 6
+            %w(P Q R S), # 7
+            %w(T U V),   # 8
+            %w(W X Y Z)  # 9
+        ]
+
     numlen = num.length - 1
     0.upto(numlen) do |i|
-        letters = [
-            %w(A B C),   # 0
-            %w(D E F),   # 1
-            %w(G H I),   # 2
-            %w(J K L),   # 3
-            %w(M N O),   # 4
-            %w(P Q R S), # 5
-            %w(T U V),   # 6
-            %w(W X Y Z)  # 7
-        ]
- 
-        letters.each_with_index do |j, index_j|
-            j.each_with_index do |k, index_k|
-                if num[i].upcase == letters[index_j][index_k]
-                    newnum = index_j + 2
-                    num[i] = newnum.to_s
+        letters.each_with_index do |line, index_line|
+            line.each_with_index do |letter, index_letter|
+                if num[i].upcase == letters[index_line][index_letter]
+                    num[i] = index_line.to_s
                 end
             end          
         end
     end
     num
 end
-
 
 puts phone_conv('555-MATRESS') # => 555-6287377
